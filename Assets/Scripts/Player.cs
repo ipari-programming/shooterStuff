6a02b29890261187908306a163d5f48c238f1433
Csáki <jockahun@gmail.com>
2018-09-22 15:41:11 +0200

    Added joysticks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TouchControlsKit;
 
 public class Player : MonoBehaviour {
     
     public Sprite skinIdle;
     public Sprite skinRun;
     public Sprite skinAttack;
+
+    public TCKJoystick joystickMove;
 
     public float speed = 10f;
 
@@ -21,8 +24,8 @@ public class Player : MonoBehaviour {
     }
 
     void Update () {
-        float moveH = Input.GetAxis("Horizontal");
-        float moveV = Input.GetAxis("Vertical");
+        float moveH = joystickMove.axisX.value;
+        float moveV = joystickMove.axisY.value;
 
         rb.AddForce(new Vector2(moveH * speed * 10, moveV * speed * 10));
 
