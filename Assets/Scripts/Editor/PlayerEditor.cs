62814f3d1dd78f67de6970e80558f1415ca41f50
Csáki <jockahun@gmail.com>
2018-09-23 10:27:59 +0200

    Added player editor and modified characters

---
+++ Assets/Scripts/Editor/PlayerEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using TouchControlsKit;
+
+[CustomEditor(typeof(Player))]
+public class PlayerInspector : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        Player player = (Player)target;
+
+        player.joystickMove = (TCKJoystick)EditorGUILayout.ObjectField("Joystick Move", player.joystickMove, typeof(TCKJoystick), true);
+        player.joystickShoot = (TCKJoystick)EditorGUILayout.ObjectField("Joystick Shoot", player.joystickShoot, typeof(TCKJoystick), true);
+    }
+
+}
