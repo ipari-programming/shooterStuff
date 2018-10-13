e6b8fd9c940ebad966fc27ecc55dbb4b33b76aa1
Csáktornyai Ádám <jockahun@gmail.com>
2018-10-13 16:25:48 +0200

    Added menu with player selector

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public Image header;
+    public Text title;
+
+    public SpriteRenderer characterDisplay;
+
+    public Button buttonRight;
+    public Button buttonLeft;
+
+    public Character[] characters;
+
+    int characterID = 0;
+
+	void Start ()
+    {
+        SelectCharacter(PlayerPrefs.GetInt("last-player", 0));
+
+        buttonRight.onClick.AddListener(() =>
+        {
+            if (characterID < characters.Length - 1) characterID++;
+            SelectCharacter(characterID);
+        });
+        buttonLeft.onClick.AddListener(() =>
+        {
+            if (characterID > 0) characterID--;
+            SelectCharacter(characterID);
+        });
+    }
+
+    void SelectCharacter(int id)
+    {
+        header.color = characters[id].mainColor;
+        title.text = characters[id].name;
+
+        characterDisplay.sprite = characters[id].skinIdle;
+    }
+
+    public void StartGame()
+    {
+        PlayerPrefs.SetInt("last-player", characterID);
+
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public Player player;
+
+    public Character[] characters;
+
+    void Start()
+    {
+        player.GetComponent<CharacterDisplay>().character = characters[PlayerPrefs.GetInt("last-player", 0)];
+        player.GetComponent<CharacterDisplay>().ChangeCharacter();
+    }
+}
