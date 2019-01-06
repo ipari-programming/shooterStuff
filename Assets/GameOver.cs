41e6dcadaaca427e4dd7dd628ef5f3cc2c60c9d7
Csáktornyai Ádám <jockahun@gmail.com>
2019-01-06 22:15:00 +0100

    Game over? Added new scene

---
+++ Assets/GameOver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+
+    AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+}
