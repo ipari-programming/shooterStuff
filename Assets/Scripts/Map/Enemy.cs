c322e544ac1741e3fa04871ec2f764d62a291745
Csáktornyai Ádám <jockahun@gmail.com>
2019-03-11 20:30:05 +0100

    Splitted enemy script and made a few things ready for more enemies

--- Assets/Scripts/Map/Enemy.cs
+++ Assets/Scripts/Map/Enemy.cs
@@ -3,151 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
-
-    enum AiActivity { wander, attack }
-
-    public float health;
-    public float damage;
-    public float speed;
-    public float range;
-    public float attackSpeed;
-
-    Player player;
-
-    Rigidbody2D rb;
-
-    Animator animator;
-
-    AiActivity aiActivity;
-
-    Vector3 destination, prevPos;
-
-    bool positionReached = false;
-    bool crAttack = false;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-
-        animator = GetComponent<Animator>();
-
-        Physics2D.queriesStartInColliders = false;
-
-        aiActivity = AiActivity.wander;
-
-        destination = transform.position;
-    }
-
-    void Update()
-    {
-        LookForPlayer();
-
-        animator.SetFloat("speed", rb.velocity.magnitude);
-        animator.SetBool("attack", crAttack);
-
-        switch (aiActivity)
-        {
-            case AiActivity.wander:
-
-                Vector2 rdmLocation = new Vector2(
-                    transform.position.x + Random.Range(-3, 3),
-                    transform.position.y + Random.Range(-3, 3)
-                );
-
-                if (positionReached)
-                {
-                    destination = rdmLocation;
-                }
-
-                break;
-            case AiActivity.attack:
-                
-                destination = player.transform.position;
-
-                if (Vector3.Distance(transform.position, player.transform.position) <= range)
-                {
-                    if (!crAttack) StartCoroutine(Attack());
-                }
-
-                break;
-        }
-    }
     
-    // TODO need to optimize
-    void LookForPlayer()
+    public float health;
+    public float damageOnCollide = 0;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (player == null) player = FindObjectOfType<Player>();
-        else
-        {
-            for (float i = 0; i < Mathf.PI; i += Mathf.PI / 16)
-            {
-                float angle = i - (Mathf.PI / 2) - (transform.eulerAngles.z / 180 * Mathf.PI);
+        if (!collision.gameObject.GetComponent<Player>()) return;
 
-                Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+        Player player = collision.gameObject.GetComponent<Player>();
 
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
-
-                if (hit)
-                {
-                    if (hit.transform == player.transform)
-                    {
-                        Debug.DrawLine(transform.position, hit.point, Color.red);
-
-                        ChangeActivity(AiActivity.attack, true);
-
-                        break;
-                    }
-                    else
-                    {
-                        Debug.DrawLine(transform.position, hit.point, Color.green);
-
-                        ChangeActivity(AiActivity.wander, false);
-                    }
-                }
-            }
-        }
-
-        #region Movement
-
-        rb.velocity = Vector3.Normalize(destination - transform.position) * speed * Time.deltaTime * 10;
-        transform.up = Vector3.Lerp(transform.up, rb.velocity, Time.deltaTime * speed);
-
-        if (Vector3.Distance(destination, transform.position) < .1f || Vector3.Distance(prevPos, transform.position) < .005f)
-        {
-            positionReached = true;
-        }
-        else
-        {
-            positionReached = false;
-        }
-
-        prevPos = transform.position;
-
-        #endregion
+        player.DealDamage(damageOnCollide);
     }
 
-    void ChangeActivity(AiActivity newActivity, bool force)
-    {
-        aiActivity = newActivity;
-    }
-
-    IEnumerator Attack()
-    {
-        crAttack = true;
-        
-        while (player != null && Vector3.Distance(transform.position, player.transform.position) <= range)
-        {
-            player.DealDamage(damage);
-
-            yield return new WaitForSeconds(1 / attackSpeed);
-
-            if (player == null) break;
-        }
-        
-        crAttack = false;
-    }
-
-    #region Health and death
     public bool DealDamage(float amount)
     {
         health -= amount;
@@ -163,6 +31,4 @@ public class Enemy : MonoBehaviour {
     {
         Destroy(gameObject);
     }
-    #endregion
-
 }
